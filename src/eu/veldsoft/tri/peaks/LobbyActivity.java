5d2b5211139a2f3663eb3cce4d67e39bf84290d5
VictorVangelov <victor.vangelov@gmail.com>
2014-08-01 00:45:52 +0300

    Cards are dealeds.

--- src/eu/veldsoft/tri/peaks/LobbyActivity.java
+++ src/eu/veldsoft/tri/peaks/LobbyActivity.java
@@ -1,9 +1,10 @@
 package eu.veldsoft.tri.peaks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LobbyActivity extends Activity {
 
@@ -11,5 +12,23 @@ public class LobbyActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lobby);
+
+		((Button) findViewById(R.id.button1))
+				.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						LobbyActivity.this.startActivity(new Intent(LobbyActivity.this, GameActivity.class));
+					}
+				});
+
+		((Button) findViewById(R.id.button5))
+				.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						finish();
+					}
+				});
 	}
 }
